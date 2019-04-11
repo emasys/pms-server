34821ac6e835ec6f8cb459e8fd9427d71ad0023f
Emmanuel Ndukwe <emasysnd@gmail.com>
2019-04-11 12:47:53 +0100

    test endpoints

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ const options = {
   relativeTo: __dirname,
 };
 
+const testResource = {};
+
 (async () => {
   try {
     const server = await Glue.compose(
       config,
       options,
     );
+    testResource.server = server;
     await server.start();
     Log(`Server running at: ${server.info.uri}`);
   } catch (error) {
@@ -19,3 +22,5 @@ const options = {
     process.exit(1);
   }
 })();
+
+export default testResource;
